589fba9d8b5bf76bd778611d5d10bcdfe94927f5
ArsecTech <arshia.ghaffarian003@gmail.com>
2022-06-13 22:45:38 +0430

    src main

---
+++ packages/main/src/index.ts
@@ -0,0 +1,92 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+import { URL } from 'url';
+
+
+const isSingleInstance = app.requestSingleInstanceLock();
+
+if (!isSingleInstance) {
+  app.quit();
+  process.exit(0);
+}
+
+app.disableHardwareAcceleration();
+ 
+const env = import.meta.env;
+
+ 
+if (env.MODE === 'development') {
+  app.whenReady()
+    .then(() => import('electron-devtools-installer'))
+    .then(({ default: installExtension, VUEJS3_DEVTOOLS }) => installExtension(VUEJS3_DEVTOOLS, {
+      loadExtensionOptions: {
+        allowFileAccess: true,
+      },
+    }))
+    .catch(e => console.error('Failed install extension:', e));
+}
+
+let mainWindow: BrowserWindow | null = null;
+
+const createWindow = async () => {
+  mainWindow = new BrowserWindow({
+    show: false, // Use 'ready-to-show' event to show window
+    vibrancy: 'under-window',
+    visualEffectState: 'active',
+    webPreferences: {
+      preload: join(__dirname, '../../preload/dist/index.cjs'),
+      contextIsolation: env.MODE !== 'test',   // Spectron tests can't work with contextIsolation: true
+      enableRemoteModule: env.MODE === 'test', // Spectron tests can't work with enableRemoteModule: false
+    },
+  });
+
+  
+  mainWindow.on('ready-to-show', () => {
+    if (!mainWindow?.isVisible()) {
+      mainWindow?.show();
+    }
+
+    if (env.MODE === 'development') {
+      mainWindow?.webContents.openDevTools();
+    }
+  });
+
+  
+  const pageUrl = env.MODE === 'development'
+    ? env.VITE_DEV_SERVER_URL
+    : new URL('../renderer/dist/index.html', 'file://' + __dirname).toString();
+
+
+  await mainWindow.loadURL(pageUrl);
+};
+
+
+app.on('second-instance', () => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
+  }
+});
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+
+app.whenReady()
+  .then(createWindow)
+  .catch((e) => console.error('Failed create window:', e));
+
+
+// Auto-updates
+if (env.PROD) {
+  app.whenReady()
+    .then(() => import('electron-updater'))
+    .then(({ autoUpdater }) => autoUpdater.checkForUpdatesAndNotify())
+    .catch((e) => console.error('Failed check updates:', e));
+}
+
